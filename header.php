22d0afc041ad7e693adaf20b044318daccff7f62
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-03-26 17:09:57 -0400

    s7c2 redesign du menu mobile

--- header.php
+++ header.php
@@ -16,11 +16,10 @@
     <div id="menu" class="global bck-primaire-300">
         <?= get_custom_logo(); ?>
         <header class="entete__menu">
+            <input id="chk_burger" type="checkbox">
             <label id="burger" for="chk_burger">
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
             </label>
-            <input id="chk_burger" type="checkbox">
             <?php wp_nav_menu(array("container"=>"nav")); ?>
-            <div class="trait"></div>
         </header>
     </div>
