e36f79fa88f457822e2ce0d905d7531de123a690
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-03-26 01:19:02 -0400

    s7c2 affichage et formatage des catégories sur frontpage.php

--- front-page.php
+++ front-page.php
@@ -15,34 +15,45 @@
 </div>
 <div id="accueil" class="global bck-primaire-200 clr-primaire-400">
         <section class="accueil__section">
-            <h2>Accueil (h2)</h2>
+            <h2>Catégories</h2>
         <div class="section__cours">  
- <?php
-  /*
-        if (have_posts()){
-            while(have_posts()){
-                the_post();
-                the_title('<p>','</p>');
-                $contenu = get_the_content();
-                $contenu = wp_trim_words($contenu, 10);
-                echo $contenu;
-            }
-        }
-  */
-    // the_title() // echo du titre
-    // get_the_title // string du titre
-  ?>      
-  <?php if (have_posts()):
-        while(have_posts()): the_post(); ?>
-        <div class="carte">
-            <h4><?php the_title() ?></h4>
-            <p><?= wp_trim_words(get_the_content(),10); ?></p>
-            <p><a href="<?= get_permalink(); ?>">Lire plus</a> </p>
-            <?php the_category() ?>
+            <!-- Affichage des catégories de voyages -->
+            <?php
+                $categories = get_categories();
+                foreach ($categories as $cat) {
+                    $noms[] = $cat->name;
+                    $desc[] = $cat->description;
+                    $slug[] = $cat->slug;
+                    $nb[] = $cat->count;
+                    // print_r($cat);
+                }
+                foreach($categories as $cat) : ?>
+                <div class="carte">
+                    <div class="titre">
+                        <h4><?= $cat->name; ?></h4>
+                        <p><a href="<?= "category/".$cat->slug; ?>">Lire Plus</a></p>
+                    </div>
+                    <p><?= wp_trim_words($cat->description, 10); ?></p>
+                    <h5><?= $cat->count; ?> destinations</h5>
+                </div>
+                <?php endforeach; ?>
+                <!-- Affichage des destination populaires  -->
         </div>
-       <?php endwhile; ?>
-    <?php endif; ?>
-</div>
+        <h2>Destinations Populaires</h2>
+        <div class="section__cours">
+            <?php if (have_posts()):
+                // the_title() // echo du titre
+                // get_the_title // string du titre
+                while(have_posts()): the_post(); ?>
+                <div class="carte">
+                    <h4><?php the_title() ?></h4>
+                    <p><?= wp_trim_words(get_the_content(),10); ?></p>
+                    <p><a href="<?= get_permalink(); ?>">Lire plus</a> </p>
+                    <?php the_category() ?>
+                </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
 
     </div>
     <div id="galerie" class="global diagonal">
@@ -58,7 +69,6 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">Lorem, ipsum.</a>  Minima velit qui unde odit quae, <a href="#">Lorem, ipsum.</a>  magni labore maiores facilis obcaecati dolore, ullam facere. Ducimus veniam reprehenderit, temporibus ab at possimus fugit?</p>
              <blockquote>Événement ipsum, dolor sit amet consectetur adipisicing elit. Accusantium a, repellat alias qui ut in ratione optio quia quae minus repudiandae ducimus aliquid aperiam unde atque tempore non. Non, magnam.</blockquote>
         </section>
-        <?php get_template_part("gabarit/vague"); ?>
     </div>
 
     <?php  get_footer(); ?>
