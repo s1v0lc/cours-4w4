4a840290302064e6aa1fcc0b270d8a13ca8aff69
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-04-09 01:07:31 -0400

    s8c2 formatage footer.php & searchform.php

--- front-page.php
+++ front-page.php
@@ -56,20 +56,5 @@
         </div>
 
     </div>
-    <div id="galerie" class="global diagonal">
-        <section class="galerie__section">
-            <h2>Galerie  (h2)</h2>
-            <p>Lorem ipsum dolor sit amet,<a href="#">Lorem, ipsum.</a>  consectetur adipisicing elit. Minima <a href="#">Lorem, ipsum.</a>  velit qui unde odit quae, magni labore maiores facilis obcaecati dolore, ullam facere. Ducimus veniam reprehenderit, temporibus ab at possimus fugit?</p>
-            <blockquote>Galerie ipsum, dolor sit amet consectetur adipisicing elit. Accusantium a, repellat alias qui ut in ratione optio quia quae minus repudiandae ducimus aliquid aperiam unde atque tempore non. Non, magnam.</blockquote>
-        </section>
-    </div>
-    <div id="evenement" class="global">
-        <section class="evenement__section">
-            <h2>Événement</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">Lorem, ipsum.</a>  Minima velit qui unde odit quae, <a href="#">Lorem, ipsum.</a>  magni labore maiores facilis obcaecati dolore, ullam facere. Ducimus veniam reprehenderit, temporibus ab at possimus fugit?</p>
-             <blockquote>Événement ipsum, dolor sit amet consectetur adipisicing elit. Accusantium a, repellat alias qui ut in ratione optio quia quae minus repudiandae ducimus aliquid aperiam unde atque tempore non. Non, magnam.</blockquote>
-        </section>
-    </div>
-
     <?php  get_footer(); ?>
     
--- searchform.php
+++ searchform.php
@@ -1,11 +1,21 @@
 <?php
 /* Formulaire de recherche personnalisée */
 ?>
-<form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<!-- <form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <label>
     <input class="recherche__input" type="search" class="search-field" placeholder="Recherche..." value="<?php echo get_search_query(); ?>" name="s" />
   </label>
   <button class="recherche__bouton" type="submit" class="search-submit">
     <span class="recherche__icone">&#x1F50D;</span>
   </button>
-</form>
+</form> -->
+
+
+  <form class="recherche" role="search" method="get"  action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <label>
+      <input class="recherche__input" type="search" class="search-field" placeholder="Recherche..." value="<?php echo get_search_query(); ?>" name="s" />
+    </label>
+    <button class="recherche__button" type="submit">
+      <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+    </button>
+  </form>
