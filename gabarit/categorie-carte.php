d88b9525d73b792b3b6fbf0da529500efef7db9f
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-05-16 17:11:29 -0400

    s14c1 formatage des filtres par catégories et des cartes dans voyage.js

--- gabarit/categorie-carte.php
+++ gabarit/categorie-carte.php
@@ -3,27 +3,6 @@
  * Gabarit pour afficher carte de destinations
  */
 ?>
-<!-- Cartes catégories front-page -->
-<!-- <?php
-    $categories = get_categories();
-    foreach ($categories as $cat) {
-        $noms[] = $cat->name;
-        $desc[] = $cat->description;
-        $slug[] = $cat->slug;
-        $nb[] = $cat->count;
-    }
-?>
-<?php foreach($categories as $cat) : ?>
-    <div class="carte">
-        <div class="titre">
-            <h3><?= $cat->name; ?></h3>
-            <p><a href="<?= "category/".$cat->slug; ?>">Lire Plus</a></p>
-        </div>
-        <p><?= wp_trim_words($cat->description, 10); ?></p>
-        <h4><?= $cat->count; ?> destinations</h4>
-    </div>
-<?php endforeach; ?> -->
-
 <!-- cartes destinations pop -->
     <?php $pos = strpos(get_the_title(), ",")?>
     <?php $titre = substr(get_the_title(), 0, $pos) ?>
