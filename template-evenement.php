a22001c7c0c8a066d6250090d5ed63d0715f975a
Clovis Gauthier <e2290836@cmaisonneuve.qc.ca>
2024-05-31 22:49:26 -0400

    nettoyage final

--- template-evenement.php
+++ template-evenement.php
@@ -8,20 +8,20 @@
 <?php get_header(); ?>
 <main class="page_conference">
         <!-- Titre -->
-        <h1><?= get_the_title(); ?></h1>
-        <!-- Vague -->
-        <div class="vague__header">
-            <?php get_template_part("gabarit/vague"); ?>
+    <h1><?= get_the_title(); ?></h1>
+    <!-- Vague -->
+    <div class="vague__header">
+        <?php get_template_part("gabarit/vague"); ?>
+    </div>
+    <!-- Contenu de la page -->
+    <div class="conference__contenu">
+        <?php the_content();?>
+        <!-- ACF -->
+        <div class="conference__acf">
+            <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
+            <p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>     
         </div>
-        <!-- Contenu de la page -->
-        <div class="conference__contenu">
-            <?php the_content();?>
-            <!-- ACF -->
-            <div class="conference__acf">
-                <p>L'adresse de l'evénement: <?php the_field('adresse'); ?></p>
-                <p>La date et l'heure de l'événement: <?php the_field('date_et_heure'); ?></p>     
-            </div>
-        </div>
+    </div>
     <!-- Vague -->
     <div class="vague__footer">
         <?php get_template_part("gabarit/vague"); ?>
